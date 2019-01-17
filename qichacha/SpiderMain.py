98ef1b8e8d67a2fe89747230eada993d1d6f3b0a
553798608@qq.com <553798608@qq.com>
2019-01-17 14:14:30 +0800

    使用scrapy爬虫

--- qichacha/SpiderMain.py
+++ qichacha/SpiderMain.py
@@ -20,7 +20,7 @@ class SpiderMain(object):
         new_urls=self.parser._parse_url(root_url,html_content)
         #把根页面的链接放入链接管理器
         self.urls.add_new_urls(new_urls)
-        print("匹配到的链接数量："+self.urls.new_url_size())
+        print("匹配到的链接数量：%d"%self.urls.new_url_size())
     def craw(self):
         while self.urls.has_new_url():
             try:
@@ -43,8 +43,8 @@ class SpiderMain(object):
 if __name__ == '__main__':
     n =1
     spider = SpiderMain()
-    while n <= 170:
-        print("当前是第"+n+"页")
+    while n <= 1:
+        print("当前是第%d页"%n)
         #解析根页面获取链接地址
         spider.craw_root("https://www.qichacha.com/news_index.shtml?p=%s"%(n))
         #解析子页面
